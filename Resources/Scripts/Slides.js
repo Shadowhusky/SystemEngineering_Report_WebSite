9e9c2354f2c91545f78ed8100a7e3122b08f13c9
Richard Liao <zcablyu@ucl.ac.uk>
2020-01-15 20:44:36 +0000

    Bi-Week reports fixed

--- Resources/Scripts/Slides.js
+++ Resources/Scripts/Slides.js
@@ -1,9 +1,21 @@
-lightGallery(document.getElementById('lightgallery'),{
+lightGallery(document.getElementById('HCISlides'),{
+    mode: 'lg-fade',
+    cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
+});
+lightGallery(document.getElementById('Appendix_BiWeekReport_1_Slides'),{
+    mode: 'lg-fade',
+    cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
+});
+lightGallery(document.getElementById('Appendix_BiWeekReport_2_Slides'),{
+    mode: 'lg-fade',
+    cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
+});
+lightGallery(document.getElementById('AchievementTable'),{
     mode: 'lg-fade',
     cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
 });
 
-lightGallery(document.getElementById('AchievementTable'),{
+lightGallery(document.getElementById('MosCoW'),{
     mode: 'lg-fade',
     cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
 });
