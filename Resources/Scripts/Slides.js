e871b565393443e80031b3ad1c2242958b98e8d8
Richard Liao <zcablyu@ucl.ac.uk>
2020-04-05 10:11:28 +0100

    Added Design and System Architecture diagram

--- Resources/Scripts/Slides.js
+++ Resources/Scripts/Slides.js
@@ -35,16 +35,15 @@ lightGallery(document.getElementById('MosCoW'),{
     mode: 'lg-fade',
     cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
 });
-
+lightGallery(document.getElementById('SystemArchitectureDiagram_Slide'),{
+    mode: 'lg-fade',
+    cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
+});
 lightGallery(document.getElementById('MosCoW'),{
     mode: 'lg-fade',
     cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
 });
 lightGallery(document.getElementById('Appendix_DeploymentManual_Slides'),{
-    mode: 'lg-fade',
-    cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
-});
-lightGallery(document.getElementById('SystemArchitectureDiagram'),{
     mode: 'lg-fade',
     cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
 });
